688c73d1dd4ba6c938806492d0b1715bd12b5e9c
saifuddin24 <saif.bigc@gmail.com>
2023-01-21 20:32:29 +0600

    Express js added

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 // dependencies
-const server = require('./lib/server');
+
+const express = require('express')
+const app = express()
+const port = 213;
+const router = require('./routes/index');
+
+router.init( app )
+
+// respond with "hello world" when a GET request is made to the homepage
+// app.get('/', (req, res) => {
+//     res.send('hello world')
+// })
+
+
+//const server = require('./lib/server');
 const workers = require('./lib/worker');
 
 // app object - module scaffolding
-const app = {};
+//const app = {};
 
-app.init = () => {
-    // start the server
-    server.init();
+// app.init = () => {
+//     // start the server
+//     //server.init();
+//
+//     // start the workers
+//     workers.init();
+// };
 
-    // start the workers
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
     workers.init();
-};
-
-app.init();
+})
 
 // export the app
 module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+module.exports = { init: (app) => {
+        app.get('/', () =>{
+
+
+        })
+    }
+}
